1e23152f277965df392594293bcf63c76b8b6794
Morvana Bonin <morvana.bonin@sensedia.com>
2020-09-11 23:37:23 -0300

    WIP

--- adapter_pattern/src/DuckSimulator.java
+++ adapter_pattern/src/DuckSimulator.java
@@ -3,6 +3,13 @@ public class DuckSimulator {
     //principal class
     public static void main (String[] a)
     {
+        //Test a duck
+        Duck duck = new MallardDuck();
+        testDuck();
+    }
 
+    static void testDuck(Duck duck) {
+        duck.quack();
+        duck.fly();
     }
 }
