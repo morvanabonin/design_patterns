c7dfe45470bd0c2e36a8cefdad01b83a2e7200ad
Morvana Bonin <morvana.bonin@sensedia.com>
2020-09-12 23:27:21 -0300

    Implementing TurkeyAdapter.

--- adapter_pattern/src/DuckSimulator.java
+++ adapter_pattern/src/DuckSimulator.java
@@ -4,8 +4,14 @@ public class DuckSimulator {
     public static void main (String[] a)
     {
         //Test a duck
-        Duck duck = new MallardDuck();
+        Duck duck;
+        duck = new MallardDuck();
         testDuck(duck);
+
+        //Test a turkey
+        Turkey turkey = new WildTurkey();
+        Duck turkeyAdapter = new TurkeyAdapter(turkey);
+        testDuck(turkeyAdapter);
     }
 
     static void testDuck(Duck duck) {
